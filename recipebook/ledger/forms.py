08e02fab1bacb2b19e1db9175ea35293bb140862
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-03-12 22:20:18 +0800

    edited ImageForm

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -11,4 +11,4 @@ class RecipeForm(forms.ModelForm):
 class ImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
-        fields = '__all__'
+        fields = ['image', 'description']
